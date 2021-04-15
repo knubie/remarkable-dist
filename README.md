f22ce852276d0644a856c0b9df2094b300066a78
Matthew Steedman <steedman87@gmail.com>
2021-04-15 09:46:37 -0400

    Modify htmltag parser to include full html element

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Applies patch jonschlinkert/remarkable#378
+
+Also modifies htmltag from https://github.com/knubie/remarkable/tree/structured-htmltag
--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -6681,9 +6681,9 @@ function replace$1(regex, options) {
 
 var attr_name     = /[a-zA-Z_:][a-zA-Z0-9:._-]*/;
 
-var unquoted      = /[^"'=<>`\x00-\x20]+/;
-var single_quoted = /'[^']*'/;
-var double_quoted = /"[^"]*"/;
+var unquoted      = /([^"'=<>`\x00-\x20]+)/;
+var single_quoted = /'([^']*)'/;
+var double_quoted = /"([^"]*)"/;
 
 /*eslint no-spaced-func:0*/
 var attr_value  = replace$1(/(?:unquoted|single_quoted|double_quoted)/)
@@ -6692,21 +6692,20 @@ var attr_value  = replace$1(/(?:unquoted|single_quoted|double_quoted)/)
                     ('double_quoted', double_quoted)
                     ();
 
-var attribute   = replace$1(/(?:\s+attr_name(?:\s*=\s*attr_value)?)/)
+var attribute   = replace$1(/\s+(attr_name)(?:\s*=\s*attr_value)?/)
                     ('attr_name', attr_name)
                     ('attr_value', attr_value)
                     ();
 
-var open_tag    = replace$1(/<[A-Za-z][A-Za-z0-9]*attribute*\s*\/?>/)
+var open_tag    = replace$1(/<([A-Za-z][A-Za-z0-9]*)(?:attribute)*\s*\/?>/)
                     ('attribute', attribute)
                     ();
 
-var close_tag   = /<\/[A-Za-z][A-Za-z0-9]*\s*>/;
+var close_tag   = /<\/([A-Za-z][A-Za-z0-9]*)\s*>/;
 var comment     = /<!---->|<!--(?:-?[^>-])(?:-?[^-])*-->/;
 var processing  = /<[?].*?[?]>/;
 var declaration = /<![A-Z]+\s+[^>]*>/;
 var cdata       = /<!\[CDATA\[[\s\S]*?\]\]>/;
-
 var HTML_TAG_RE = replace$1(/^(?:open_tag|close_tag|comment|processing|declaration|cdata)/)
   ('open_tag', open_tag)
   ('close_tag', close_tag)
@@ -6725,9 +6724,39 @@ function isLetter$2(ch) {
   return (lc >= 0x61/* a */) && (lc <= 0x7a/* z */);
 }
 
+function getAttrs(element_string) {
+  // element_string is a string that looks like "<span foo='bar'>"
+  try {
+    let attrs = {};
+    const attrs_matches = [...element_string.matchAll(new RegExp(attribute, 'g'))];
+    attrs_matches.forEach(function(match) {
+      //  match[1] -> attr_name
+      //  match[2] -> attr_val unquoted
+      //  match[3] -> attr_val single_quoted
+      //  match[4] -> attr_val double_quoted
+      let val = match[2] === undefined
+        ? match[3] === undefined
+          ? match[4]
+          : match[3]
+        : match[2];
+      // undefined attr_val -> value-less attribute, assign to true.
+      attrs[match[1]] = val === undefined ? true : val;
+    });
+    return attrs
+  } catch (err) { console.error(err); return {}; }
+}
+
 
 function htmltag(state, silent) {
-  var ch, match, max, pos = state.pos;
+  var ch, max, found,
+    match_open,
+    match_close,
+    tag_name,
+    pos_after_open_tag,
+    pos_after_close_tag,
+    self_closing,
+    start = state.pos,
+    pos = state.pos;
 
   if (!state.options.html) { return false; }
 
@@ -6740,24 +6769,78 @@ function htmltag(state, silent) {
 
   // Quick fail on second char
   ch = state.src.charCodeAt(pos + 1);
-  if (ch !== 0x21/* ! */ &&
-      ch !== 0x3F/* ? */ &&
-      ch !== 0x2F/* / */ &&
-      !isLetter$2(ch)) {
+  if (!isLetter$2(ch)) {
     return false;
   }
 
-  match = state.src.slice(pos).match(HTML_TAG_RE);
-  if (!match) { return false; }
+  // Look for an open tag.
+  match_open = state.src.slice(pos).match(open_tag);
+
+  if (!match_open) { return false; }
+
+  // Tag name. "<span>" -> "span"
+  tag_name = match_open[1];
+
+  pos_after_open_tag = state.pos + match_open[0].length;
+
+  state.pos = pos_after_open_tag;
+
+  // Is the open tag self-closing?
+  self_closing = match_open[0].match(/<[A-Za-z][A-Za-z0-9]*.*\s*\/>/);
+
+  if (!silent && self_closing) {
+    state.push({
+      type: 'htmltag',
+      tag_name: tag_name,
+      attrs: getAttrs(match_open[0]),
+      level: state.level
+    });
+    return true;
+  }
+
+  // Not a self-closing tag, start looking for the closing tag.
+  while (state.pos < max) {
+    if (state.src.charCodeAt(state.pos) !== 0x3C /* < */ ||
+        state.src.charCodeAt(state.pos + 1) !== 0x2F /* / */ ||
+        state.pos + 2 >= max) {
+      state.parser.skipToken(state);
+    } else {
+      match_close = state.src.slice(state.pos).match(close_tag);
+      if (match_close && match_close[1] === tag_name) {
+        found = true;
+        break;
+      } else { state.parser.skipToken(state); }
+    }
+  }
+
+  if (!found) {
+    // parser failed to find ending tag, so it's not valid htmltag
+    state.pos = start;
+    return false;
+  }
+
+  pos = state.pos;
+  pos_after_close_tag = pos + match_close[0].length;
+
+  // found!
+  state.posMax = pos;
+  state.pos = pos_after_open_tag;
 
   if (!silent) {
     state.push({
-      type: 'htmltag',
-      content: state.src.slice(pos, pos + match[0].length),
-      level: state.level
+      type: 'htmltag_open',
+      tag_name: tag_name,
+      attrs: getAttrs(match_open[0]),
+      level: state.level++
+    });
+    state.parser.tokenize(state);
+    state.push({
+      type: 'htmltag_close',
+      level: --state.level
     });
   }
-  state.pos += match[0].length;
+  state.pos = pos_after_close_tag;
+  state.posMax = max;
   return true;
 }
 
